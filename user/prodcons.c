44708d63c436618408da27104b018ea7c41e5c25
Joel D'Autilio <joeldautilio@gmail.com>
2024-05-22 11:11:55 -0300

    file prodcons
    Co-authored-by: Pablo Rossi <pablorossi2001@gmail.com>

--- user/prodcons.c
+++ user/prodcons.c
@@ -1,106 +1,115 @@
+#include "kernel/param.h"
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
 #include "kernel/fs.h"
 #include "kernel/fcntl.h"
+#include "kernel/syscall.h"
+#include "kernel/memlayout.h"
+#include "kernel/riscv.h"
+#include "prodcons.h"
 
-#define NPRODS 5
-#define NCONS 5
-#define BUFSIZE 10
+int bufferd;
+char numstr[5] = "0000";
 
-#define FULLKEY 0
-#define EMPTYKEY 1
-#define MUTEXKEY 2
-
-struct {
-    char array[BUFSIZE];
-    int head;
-    int tail;
-    int size;
-} *buffer;
-
-void bufinit() {
-    buffer = malloc(sizeof(*buffer));
-    buffer->head = 0;
-    buffer->tail = 0;
-    buffer->size = 0;
+char inc(char c) {
+  c++;
+  if (c == '9' + 1) {
+    c = '0';
+  }
+  return c;
 }
 
-void enqueue(char c) {
-    buffer->array[buffer->tail] = c;
-    printf("enqueue: %c\n", c);
-    buffer->tail = (buffer->tail + 1) % BUFSIZE;
-    buffer->size++;
+char dec(char c) {
+  c--;
+  if (c == '0' - 1) {
+    c = '9';
+  }
+  return c;
 }
 
-char dequeue() {
-    char c = buffer->array[buffer->head];
-    printf("dequeue: %c\n", c);
-    buffer->head = (buffer->head + 1) % BUFSIZE;
-    buffer->size--;
-    return c;
+void increment() {
+  for (int i = 3; i >= 0; i--) {
+    numstr[i] = inc(numstr[i]);
+    if (numstr[i] != '0') break;
+  }
+}
+
+void decrement() {
+  for (int i = 3; i >= 0; i--) {
+    numstr[i] = dec(numstr[i]);
+    if (numstr[i] != '9') break;
+  }
 }
 
 int main(int argc, char *argv[]) {
-    int full_semid = semcreate(FULLKEY, BUFSIZE);
-    int empty_semid = semcreate(EMPTYKEY, 0);
-    int mutex_semid = semcreate(MUTEXKEY, 1);
-    int i;
+  // Initialize the buffer file
+  bufferd = open(BUFNAME, O_CREATE|O_WRONLY|O_TRUNC);
+  write(bufferd, "0000\n", 5);
+  close(bufferd);
 
-    bufinit();
-    
-    for (i = 0; i < NPRODS; i++) {
-        if (fork() == 0) {
-            full_semid = semget(FULLKEY);
-            empty_semid = semget(EMPTYKEY);
-            mutex_semid = semget(MUTEXKEY);
-            
-            semwait(full_semid);
-            semwait(mutex_semid);
-            
-            char c = 'a';
-            enqueue(c);
-            //printf("prod %d: %c\n", getpid(), c);
+  int full_semid = semcreate(FULLKEY, BUFSIZE);
+  int empty_semid = semcreate(EMPTYKEY, 0);
+  int mutex_semid = semcreate(MUTEXKEY, 1);
+  int i;
 
-            semsignal(mutex_semid);
-            semsignal(empty_semid);
+  for (i = 0; i < NPRODS; i++) {
+    if (fork() == 0) {
+      full_semid = semget(FULLKEY);
+      empty_semid = semget(EMPTYKEY);
+      mutex_semid = semget(MUTEXKEY);
 
-            semclose(full_semid);
-            semclose(empty_semid);
-            semclose(mutex_semid);
-            return 0;
-        }
+      semwait(full_semid);
+      semwait(mutex_semid);
+
+      bufferd = open(BUFNAME, O_RDONLY);
+      read(bufferd, numstr, 4);
+      increment();
+      close(bufferd);
+      bufferd = open(BUFNAME, O_WRONLY|O_TRUNC);
+      write(bufferd, numstr, 4);
+      printf("prod %d: %s\n", getpid(), numstr);
+
+      semsignal(mutex_semid);
+      semsignal(empty_semid);
+
+      return 0;
     }
+  }
 
-    for (i = 0; i < NCONS; i++) {
-        if (fork() == 0) {
-            full_semid = semget(FULLKEY);
-            empty_semid = semget(EMPTYKEY);
-            mutex_semid = semget(MUTEXKEY);
+  for (i = 0; i < NCONS; i++) {
+    if (fork() == 0) {
+      full_semid = semget(FULLKEY);
+      empty_semid = semget(EMPTYKEY);
+      mutex_semid = semget(MUTEXKEY);
 
-            semwait(empty_semid);
-            semwait(mutex_semid);
+      semwait(empty_semid);
+      semwait(mutex_semid);
 
-            //char c = 
-            dequeue();
-            //printf("cons %d: %c\n", getpid(), c);
-            
-            semsignal(mutex_semid);
-            semsignal(full_semid);
+      bufferd = open(BUFNAME, O_RDONLY);
+      read(bufferd, numstr, 4);
+      decrement();
+      close(bufferd);
+      bufferd = open(BUFNAME, O_WRONLY|O_TRUNC);
+      write(bufferd, numstr, 4);
+      printf("cons %d: %s\n", getpid(), numstr);
 
-            semclose(full_semid);
-            semclose(empty_semid);
-            semclose(mutex_semid);
-            return 0;
-        }
+      semsignal(mutex_semid);
+      semsignal(full_semid);
+
+      semclose(full_semid);
+      semclose(empty_semid);
+      semclose(mutex_semid);
+      return 0;
     }
+  }
 
-    for (i = 0; i < NPRODS + NCONS; i++) {
-        wait(0);
-    }
+  for (i = 0; i < NPRODS + NCONS; i++) {
+    wait(0);
+  }
 
-    semclose(full_semid);
-    semclose(empty_semid);
-    semclose(mutex_semid);
-    return 0;
+  semclose(full_semid);
+  semclose(empty_semid);
+  semclose(mutex_semid);
+  return 0;
 }
---
+++ user/prodcons.h
@@ -0,0 +1,9 @@
+#define NPRODS 5
+#define NCONS 5
+#define BUFSIZE 10
+
+#define BUFNAME "buffer"
+
+#define FULLKEY 0
+#define EMPTYKEY 1
+#define MUTEXKEY 2
